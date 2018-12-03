68b6f90e0d6f286f8c489d530cd0aae10a94bc09
sadydai <daishuang@geetest.com>
2018-12-03 14:31:48 +0800

    version

--- dist/gee.js
+++ dist/gee.js
@@ -23266,7 +23266,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '2.11.2',
+    version: '2.11.3',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
